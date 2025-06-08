ed8b49dbef08ab4b937b5d418b1b99088074bc21
PratyayDhond_ROG_Ubuntu <dhondpratyay@gmail.com>
2025-06-08 13:27:56 +0530

    Backend update for sending content data to frontend instead of pdf zips

--- server.js
+++ server.js
@@ -119,6 +119,29 @@ app.post('/user-highlights', upload.single('file'), async (req, res) => {
   });
 });
 
+app.post('/get-user-highlights-json', upload.single('file'), (req, res) => {
+  const file = req.file;
+  console.log('Inside get-user-highlights-json');
+  console.log('File:', file);
+  console.log('File path:', file.path);
+  console.log('File originalname:', file.originalname);
+  if (!file) return res.status(400).json({ message: 'No file uploaded' });
+  const filePath = path.join(__dirname, file.path);
+  console.log('File uploaded:', filePath);
+  var highlights = []
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading uploaded file:', err);
+      return res.status(500).json({ message: 'Error reading file' });
+    }
+    highlights = parseHighlights.parseHighlights(data); // Call the parsing function
+     fs.unlink(filePath, () => {});
+  console.log('Highlights:', highlights);
+  return res.json({ message: 'Highlights processed successfully', highlights: highlights });
+  });
+  
+});
+
 app.get('/download-highlights/:jobId', (req, res) => {
   const jobId = req.params.jobId;
   const highlightsZipPath = `./${jobId}.zip`; // Assuming the zip is named with jobId
