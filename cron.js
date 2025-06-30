d356324aa37ac47df5dded401ae29cab62722707
PratyayDhond_ROG_Ubuntu <dhondpratyay@gmail.com>
2025-06-30 22:05:33 +0530

    CRON JOB update to mitigate 503 on backend

--- cron.js
+++ cron.js
@@ -3,18 +3,27 @@ const https = require('https');
 const http = require('http');
 
 const backendUrl = process.env.BACKEND_URL || 'localhost:3000';
-const job = new cron.CronJob('*/14 * * * *', function () {
-  console.log('Keeping Server Alive - cron job running every 14 minutes');
 
-  https.get(backendUrl, (res) => {
-    if(res.statusCode === 200) {
+function keepAliveWithRetry(url) {
+  https.get(url, (res) => {
+    if (res.statusCode === 200) {
       console.log('Server is alive');
+    } else if (res.statusCode !== 200) {
+      console.error('Server did not respond. Retrying in 1 minute...');
+      setTimeout(() => keepAliveWithRetry(url), 60 * 1000);
     } else {
       console.error(`Server responded with status code: ${res.statusCode}`);
     }
   }).on('error', (err) => {
     console.error('Error keeping server alive:', err.message);
+    // Optionally retry on network errors as well:
+    setTimeout(() => keepAliveWithRetry(url), 60 * 1000);
   });
+}
+
+const job = new cron.CronJob('*/14 * * * *', function () {
+  console.log('Keeping Server Alive - cron job running every 14 minutes');
+  keepAliveWithRetry(backendUrl);
 });
 
 module.exports = {
