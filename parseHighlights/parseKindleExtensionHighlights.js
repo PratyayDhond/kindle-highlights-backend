dcf204dd8a543eb5c29f63d2283175f9cefa43da
zoro - Pratyay Dhond <dhondpratyay@gmail.com>
2025-12-25 23:31:47 +0530

    removed not needed logs

--- parseHighlights/parseKindleExtensionHighlights.js
+++ parseHighlights/parseKindleExtensionHighlights.js
@@ -90,7 +90,7 @@ function parseHighlights(fileContent) {
 function removeRedundantHighlights(books){
     let totalHighlights = 0;
     books.forEach(book => {
-        console.log('Removing redundant highlights for book:', book.name);
+        // console.log('Removing redundant highlights for book:', book.name);
         book.highlights = purgeOverlappingHighlights(book.highlights);
         totalHighlights += book.highlights.length;
     });
@@ -152,7 +152,7 @@ function purgeOverlappingHighlights(highlights) {
             locationCounter++;
         }
     }
-    console.log(locationCounter - 1, "unique highlights retained");
+    // console.log(locationCounter - 1, "unique highlights retained");
     return updatedHighlights;
 }
 
